6f0f3848b8b10e5199ea34f5da5bd5e715ad97ee
SnowOwl <mknoll1901@gmail.com>
2022-07-09 10:27:40 +0200

    test gh-pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,6 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  target: 'static',
-  router: {
-    base: '/Portfolio/'
-  },
   css: [
     '@fortawesome/fontawesome-svg-core/styles.css'
   ],
